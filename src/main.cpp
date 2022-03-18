21c7f3c81918eda1876c3b9abd438e207e7ce9a6
klesyon castro <kleysoncastro99@gmail.com>
2022-03-18 13:26:04 -0300

    ajustes de variavel 	envio de dados para master int

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,63 @@
 /* 
-  main file conication slave spi
+  main file comunication slave spi
 
  */
 
 #include <Arduino.h>
 #include <SPI.h>
 #define PIN3_INT_1 3
-#define SLAVE 10
+#define SLAVE_SENSOR_1 10
 
-void incpulso1();
+void increment_pulse_sensor_1();
+void send_to_master_vasao(float vasao);
 char buf = 15;
-unsigned char  vazao1;                                              // declara variáveis para leitura das vazões
-int contaPulso1;                                           // declara as funções para contagem dos pulsos de cada rotâmetro
+float vazao_sensor_1;
+unsigned int total_pulse_1_second;
+
+
 
 void setup() {
 
-  digitalWrite(SLAVE, HIGH);
+  digitalWrite(SLAVE_SENSOR_1, HIGH);
   SPI.begin();
 
 
-  Serial.begin(9600);                         // habilita serial 0 e sua taxa de trabalho
+  Serial.begin(9600);
   pinMode(PIN3_INT_1, INPUT);                          // configura pino 3 para receber sinal e chamar interrupção 1
-  pinMode(21, INPUT);                         // configura pino 21 para receber sinal e chamar interrupção 2
-
-  attachInterrupt(1, incpulso1, RISING);      // habilita chamada à primeira interrupção (rotâmetro 1)
+  attachInterrupt(1, increment_pulse_sensor_1, RISING);      // habilita chamada à primeira interrupção (rotâmetro 1)
 
 }
 
 void loop() {
   
-  contaPulso1 = 0;               // zera os contadores de pulsos
+  total_pulse_1_second = 0;
   sei();          // habilita interrupção
   delay(1000);                                    // aguarda 1 segundo entre leituras
   cli();          // desabilita interrupção
 
-  vazao1 = contaPulso1 / 10.7;                     // equação para conversão da contagem de pulsos em fluxo (L/min) para rot. 1
-                                                  // espera-se que os fluxos não passem de 500 L/min
-  Serial.println(vazao1);                         // envia fluxo 1 para receptor MEGA
+/* 
+  Equação para conversão da contagem de pulsos em fluxo (L/min) para rot. 1
+  espera-se que os fluxos não passem de 500 L/min
+ */
+  Serial.println(total_pulse_1_second); // linha para ser removida
 
-  digitalWrite(SLAVE, LOW);
-  SPI.transfer(buf);
-  delay(100);
+  vazao_sensor_1 = total_pulse_1_second / 10.7;                    
+                                                  
+  Serial.println(vazao_sensor_1); // linha para ser removida
+
+  send_to_master_vasao(vazao_sensor_1);
 
 }
 // rotina chamada pela interrupção provocada pelo rotâmetro 1
-void incpulso1() {
-  contaPulso1++;        // incrementa variável contando os pulsos do sensor 1
+void increment_pulse_sensor_1() {
+  total_pulse_1_second++;
+}
+
+void send_to_master_vasao(float vasao) {
+
+  digitalWrite(SLAVE_SENSOR_1, LOW);
+  SPI.transfer(4.3);
+  delay(5); // em teste
+  digitalWrite(SLAVE_SENSOR_1, HIGH);
+  delay(100);
 }
